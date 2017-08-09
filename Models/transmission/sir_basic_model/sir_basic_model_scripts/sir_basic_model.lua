831fd240a5242e09dea418cb93d8b6e93dcdac0a
tiagofml <tiagofml@gmail.com>
2017-08-09 14:48:46 -0300

    Update SIR basic model to TerraME 2.0-RC-4. Issue #14
    Update SIR basic model to TerraME 2.0-RC-4.
    No changes in the model were required. Only few changes in description
    were made.
    Fixes #14

--- Models/transmission/sir_basic_model/sir_basic_model_scripts/sir_basic_model.lua
+++ Models/transmission/sir_basic_model/sir_basic_model_scripts/sir_basic_model.lua
@@ -2,10 +2,10 @@
 -- @id sir_basic_model
 -- @name SIR Basic Model
 -- @version 1.0
--- @interpreter TerraME 2.0-beta4
--- @example A Susceptible-Infected-Recovered basic (SIR) model. 
+-- @interpreter TerraME 2.0-RC-4
+-- @example A Susceptible-Infected-Recovered (SIR) basic model. 
 -- @authors Tiago Lima, Lucas Saraiva
--- @description For a description of such model visit <<http://en.wikipedia.org/wiki/Epidemic_model>>
+-- @description For a description of such model, please visit <<http://en.wikipedia.org/wiki/Epidemic_model>>
 
 world = Model {
    Sh = Sh,
