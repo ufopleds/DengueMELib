0e229dc3f4a1f4e8bfa8bc4e033f9acd954089f8
tiagofml <tiagofml@gmail.com>
2017-08-17 12:01:28 -0300

    Develop a corresponding version in R of the SIR_basic_model. Issue #17
    Develop a corresponding version in R of the sir_basic_model.
    Fixes #17

--- Models/transmission/r_sir_basic_model/r_sir_basic_model_scripts/r_sir_basic_model.R
+++ Models/transmission/r_sir_basic_model/r_sir_basic_model_scripts/r_sir_basic_model.R
@@ -1,140 +1,54 @@
-# Load OR download deSolve package
-if(any(installed.packages() == "deSolve")){
-  require(deSolve)
-}else{
-  install.packages("deSolve")
-  require(deSolve)
+# DengueME Models Library
+# @id r_sir_basic_model
+# @name SIR Basic Model
+# @version 1.0
+# @interpreter R v3.4.1
+# @example A Susceptible-Infected-Recovered (SIR) basic model
+# @authors Fellipe Prado, Tiago Lima
+# @description For a description of such model, please visit http://en.wikipedia.org/wiki/Epidemic_model
+
+# DEPENDENCIES
+library('deSolve')
+
+# ODE SIR function
+odeSIR <- function(times, state, parameters){
+
+   with(as.list(c(state, parameters)), {
+      
+      Nh <- Sh + Ih + Rh
+      
+      dSh <- -beta * ( (Sh * Ih)/Nh )
+      dIh <- beta * ( (Sh * Ih)/Nh ) - (gamma * Ih)
+      dRh <- gamma * Ih
+      
+      return(list(c(dSh, dIh, dRh), Nh = Nh))
+   })
 }
 
-# SIR function
-sir <- function(times, state, parameters) {
-  
-  with(as.list(c(state, parameters)), {
-    
-    Nh <-  (Sh + Ih + Rh)
-    new_infections <- (beta * ((Sh * Ih)/Nh))
-    
-    if(new_infections > Sh) new_infections = Sh
-    
-    dSh <- -new_infections
-    dIh <-  new_infections - (gamma * Ih)
-    dRh <-  gamma * Ih
-    
-    return(list(c(dSh, dIh, dRh)))
-  })
-}
-
-# Set parameters
-## Proportion in each compartment: Susceptible, Infected, Recovered
-init       <- c(Sh = Sh, Ih = Ih, Rh = Rh)
-## beta: infection parameter; gamma: recovery parameter
+# Initial conditions of the population
+init <- c(Sh = Sh, Ih = Ih, Rh = Rh)
 parameters <- c(beta = beta, gamma = gamma)
-## Time frame
-times = times
-## Solve using ode (General Solver for Ordinary Differential Equations)
-out <- ode(y = init, times = times, func = sir, parms = parameters)
-## change results to data frame
-out <- as.data.frame(out)
-## Delete time variable from the results
-out$time <- NULL
+times <- seq(from = 0, to = steps, by = 1)
 
+simulation <- as.data.frame(lsoda(y = init, times = times, func = odeSIR, parms = parameters))
+head(simulation)
+tail(simulation)
 
-# RESULTS
-## Check to see if there's a desired output
-if(output){
-  ### Chart
-  if(outChart){
-    
-    outChartData = out
-    
-    if(any(outChartSelect == "Nh")){
-      outChartData <- cbind(outChartData, rowSums(outChartData))
-      colnames(outChartData)[4] <- "Nh"
-    }
-    
-    if(all(outChartSelect != "Sh")){
-      outChartData = outChartData$Sh <- NULL
-    }
-    
-    if(all(outChartSelect != "Ih")){
-      outChartData = outChartData$Ih <- NULL
-    }
-    
-    if(all(outChartSelect != "Rh")){
-      outChartData = outChartData$Rh <- NULL
-    }
-    
-    png(filename = outChartFile, width = 1000, height = 875, res = 113)
-    
-    matplot(x = times, y = outChartData, type = "l",
-            xlab = outChartXLabel, ylab = outChartYLabel, main = outChartTitle,
-            col = outChartColor, bty = "l", lty = 1, lwd = 2)
-    legend("right", legend = outChartLabel, lty = 1, lwd = 2, col = outChartColor, bty = "n")
-    dev.off()
-  }
-  
-  ### Text Screen
-  if(outTextScreen){
-    
-    outTextScreenData = out
-    
-    if(all(outTextScreenSelect != "Sh")){
-      outTextScreenData = outTextScreenData$Sh <- NULL
-    }
-    
-    if(all(outTextScreenSelect != "Ih")){
-      outTextScreenData = outTextScreenData$Ih <- NULL
-    }
-    
-    if(all(outTextScreenSelect != "Rh")){
-      outTextScreenData = outTextScreenData$Rh <- NULL
-    }
-    
-    print(outTextScreenData)
-  }
-  
-  ### Visual Table
-  if(outVisualTable){
-    
-    outVisualTableData = out
-    
-    if(all(outVisualTableSelect != "Sh")){
-      outVisualTableData = outVisualTableData$Sh <- NULL
-    }
-    
-    if(all(outVisualTableSelect != "Ih")){
-      outVisualTableData = outVisualTableData$Ih <- NULL
-    }
-    
-    if(all(outVisualTableSelect != "Rh")){
-      outVisualTableData = outVisualTableData$Rh <- NULL
-    }
-    
-    print(outVisualTableData)
-  }
-  
-  ### Log
-  if(outLog){
-    
-    outLogData = out
-    
-    if(any(outLogSelect == "Nh")){
-      outLogData <- cbind(outLogData, rowSums(outLogData))
-      colnames(outLogData)[4] <- "Nh"
-    }
-    
-    if(all(outLogSelect != "Sh")){
-      outLogData = outLogData$Sh <- NULL
-    }
-    
-    if(all(outLogSelect != "Ih")){
-      outLogData = outLogData$Ih <- NULL
-    }
-    
-    if(all(outLogSelect != "Rh")){
-      outLogData = outLogData$Rh <- NULL
-    }
-    
-    write.table(outLogData, file = outLogFile, row.names = FALSE, sep = outLogSeparator)
-  }
+# Results
+if (output) {
+   if (outChart) {
+      plot(x = simulation$time, y = simulation$Sh, type = outChartStyle[1], col=outChartColor[1], xlab="Time", ylab = "Humans")
+      lines(x = simulation$time, y = simulation$Ih, type = outChartStyle[2], col=outChartColor[2])
+      lines(x = simulation$time, y = simulation$Rh, type = outChartStyle[3], col=outChartColor[3])
+      lines(x = simulation$time, y = simulation$Nh, type = outChartStyle[4], col=outChartColor[4], pch=20)
+   }
+   if (outTextScreen) {
+      lastRow <- simulation[nrow(simulation),]
+      row.names(lastRow) <- NULL
+      View(lastRow[ c("time", outTextScreenSelect) ])
+   }
+   if (outVisualTable)
+      View(simulation[ c("time", outVisualTableSelect) ] )
+   if (outLog)
+      write.table(x = simulation[ c("time", outLogSelect) ], file = outLogFile, append = !outLogOverwrite, quote = FALSE, sep = outLogSeparator, col.names = TRUE)
 }
