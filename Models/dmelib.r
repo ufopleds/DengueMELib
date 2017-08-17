8bbefaa42243bfb9eec8b5adae7cdf90b9d85e00
fellipedoprado <fellipe.doprado@gmail.com>
2017-08-17 12:18:04 -0300

    Develop a corresponding version in R of the SIR_basic_model. Issue #17 (#23)
    Develop a corresponding version in R of the SIR_basic_model. Issue #17 
    
    The model was created with three files:
    1 - the XML file, used to generate the graphical interface, according to the same existing structure
    2 - an input file, in R, which will be automatically generated by DengueME, according to the XML file
    3 - the R model file itself
    
    Moreover, an additional file was added with a function to check dependencies and their versions. This function can be used by other R models.
    
    Fixes #17

---
+++ Models/dmelib.r
@@ -0,0 +1,27 @@
+dmelib_check_dependencies <- function(rv) {
+   # checking R version
+   if ((USER_RVERSION == rv))
+      cat(paste0("R version: ", USER_RVERSION))
+   else
+      warning(paste0("The R version in which the model was developed (", rv, ") is different from that you are using (", USER_RVERSION, ")"))
+   
+   listOfDependencies <- DEPENDENCIES[,1]
+   
+   # checking the version of packages installed by user and required by model
+   outDateMsg <- "The following installed dependencies (packages) has a different version from that used by model\n"
+   installedPackages <- USER_PACKINFO[USER_PACKINFO[,c("Package")] %in% listOfDependencies, c("Package", "Version")]
+   for(i in 1:nrow(installedPackages))
+      if (installedPackages[i,2] != DEPENDENCIES[which(DEPENDENCIES$lib == installedPackages[i,1]),2])
+         outDateMsg <- paste0(outDateMsg, "\t", installedPackages[i, "Package"], ": ", installedPackages[i,2], "(installed) - ", DEPENDENCIES[which(DEPENDENCIES$lib == installedPackages[i,1]),2], "(used by model)\n")
+   warning(outDateMsg)
+   
+   # checking the uninstalled packages required by model
+   uninstalledPackages <- listOfDependencies[!(listOfDependencies %in% USER_PACKINFO[,"Package"])]
+   if (length(uninstalledPackages)) {
+      warning(paste0("The following dependencies (packages) are not installed: ", paste(uninstalledPackages, collapse=", ")))
+      
+      for(i in 1:length(uninstalledPackages))
+         install.packages(uninstalledPackages[i], dependencies = TRUE)
+   }
+   
+}
