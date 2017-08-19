998f4717123e976e348de3e255ecd4610688c5d0
fellipedoprado <fellipe.doprado@gmail.com>
2017-08-19 16:09:38 -0300

    Corresponding version in R of the sirsi_basic_model. Issue #18 (#26)
    * Develop a corresponding version in R of the SIRSI_basic_model. Issue #18
    
    Develop a corresponding version in R of the SIRSI_basic_model.
    An error with dependencies was corrected in XML and input files. And
    small changes in XML, input and model files were made related to
    upper/lower case and source-coding format (e.g. blank spaces).
    Closes #18

---
+++ Models/transmission/r_sirsi_basic_model/r_sirsi_basic_model_input.R
@@ -0,0 +1,88 @@
+# DengueME Models Library
+# @id r_sirsi_basic_model
+# @name SIR-SI Basic Model
+# @version 1.0
+# @interpreter R v3.4.1
+# @example A Susceptible-Infected-Recovered-Susceptible-Infected (SIR-SI) basic model.
+# @authors Fellipe Prado, Tiago Lima
+# @description SIR-SI basic model is a simplified version of the model proposed by Nishiura (2006). For a complete description of such model please see the paper Nishiura (2006), "Mathematical and Statistical Analyses of the Spread of Dengue", Dengue Bulletin, Volume 30, 2006.
+
+# INPUT FILE
+print('LOADING INPUT FILE AND RUNNING MODEL')
+
+# DEPENDENCIES
+MODEL_RVERSION <- "3.4.1"
+DEPENDENCIES <- data.frame(lib = c("deSolve"), version = c("1.20"), stringsAsFactors = F)
+
+# PATH CONFIG
+BASE_PATH <- "D:/github/workingpath/DengueMELib/Models/transmission/r_sirsi_basic_model"
+SCRIPT_PATH <- "D:/github/workingpath/DengueMELib/Models/transmission/r_sirsi_basic_model/r_sirsi_basic_model_scripts"
+RESULTS_PATH <- BASE_PATH
+
+# checking dependencies
+USER_RVERSION <- package_version(R.version)
+USER_PACKINFO <- installed.packages(fields = c("Package", "Version"))
+source(paste0(BASE_PATH, "/../../dmelib.r"))
+dmelib_check_dependencies(MODEL_RVERSION)
+
+# PARAMETERS
+Sh <- 9999 # number of susceptible humans [1-999999999]
+Ih <- 1 # number of infectious humans [1-999999999]
+Rh <- 0 #  number of recovered humans [1-999999999]
+Nh <- 10000 # total population [1-999999999]
+Sv <- 20000 # number of susceptible vectors [1-999999999]
+Iv <- 0 # number of infectious vectors [1-999999999]
+Nv <- 20000 # total vector population [1-999999999]
+bitting <- 1 # average number of bites per mosquito per day [0-999]
+gamma <- 0.167 # human recovery rate [0-1]
+betah <- 0.4 # transmission probability from the vector to human [0-1]
+betav <- 0.4 # transmission probability from the human to vector [0-1]
+
+
+# SIMULATION
+steps <- 120 # simulation steps [0-999999999]
+
+# RESULTS
+outChart <- TRUE
+outChartSelect <- c("Sh", "Ih", "Rh", "Sv", "Iv")
+outChartLabel <- c("Susceptible (human)", "Infectious (human)", "Recovered (human)", "Susceptible (vector)", "Infectious (vector)")
+outChartStyle <- c("l", "l", "l", "o", "o")
+outChartColor <- c("blue", "red", "yellow", "blue", "red")
+outChartTitle <- ""
+outChartXLabel <- "Time"
+outChartYLabel <- ""
+
+outChartHuman <- TRUE
+outChartHumanSelect <- c("Sh", "Ih", "Rh", "Nh")
+outChartHumanLabel <- c("Susceptible", "Infectious", "Recovered", "Humam Population")
+outChartHumanStyle <- c("l", "l", "l", "o")
+outChartHumanColor <- c("blue", "red", "yellow", "black")
+outChartHumanTitle <- ""
+outChartHumanXLabel <- "Time"
+outChartHumanHYLabel <- ""
+
+outChartVector <- TRUE
+outChartVectorSelect <- c("Sv", "Iv", "Nv")
+outChartVectorLabel <- c("Susceptible", "Infectious", "Vector Population")
+outChartVectorStyle <- c("l", "l", "o")
+outChartVectorColor <- c("blue", "red", "black")
+outChartVectorTitle <- ""
+outChartVectorXLabel <- "Time"
+outChartVectorYLabel <- ""
+
+outTextScreen <- TRUE
+outTextScreenSelect <- c("Sh", "Ih", "Rh", "Nh", "Sv", "Iv", "Nv")
+
+outVisualTable <- TRUE
+outVisualTableSelect <- c("Sh", "Ih", "Rh", "Nh", "Sv", "Iv", "Nv")
+
+outLog <- TRUE
+outLogSelect <- c("Sh", "Ih", "Rh", "Nh", "Sv", "Iv", "Nv")
+outLogFile <- paste0(RESULTS_PATH, "/results.csv")
+outLogSeparator <- ";"
+outLogOverwrite <- TRUE
+
+output <- outChart | outChartHuman | outChartVector | outTextScreen | outVisualTable | outLog
+
+# LOADING MODEL
+source(paste0(SCRIPT_PATH, "\\r_sirsi_basic_model.r"))
---
+++ Models/transmission/r_sirsi_basic_model/r_sirsi_basic_model_scripts/r_sirsi_basic_model.R
@@ -0,0 +1,71 @@
+# DengueME Models Library
+# @id r_sirsi_basic_model
+# @name SIR-SI Basic Model
+# @version 1.0
+# @interpreter R v3.4.1
+# @example A Susceptible-Infected-Recovered-Susceptible-Infected (SIR-SI) basic model.
+# @authors Fellipe Prado, Tiago Lima
+# @description SIR-SI basic model is a simplified version of the model proposed by Nishiura (2006). For a complete description of such model please see the paper Nishiura (2006), "Mathematical and Statistical Analyses of the Spread of Dengue", Dengue Bulletin, Volume 30, 2006.
+
+# DEPENDENCIES
+library('deSolve')
+
+# ODE SIR-SI function
+odeSIRSI <- function(times, state, parameters) {
+
+   with(as.list(c(state, parameters)), {
+      
+      Nh <- Sh + Ih + Rh
+      
+      dSh <-  (-(betah * bitting)/Nh) * (Sh * Iv)
+      dIh <-  ((betah * bitting)/Nh) * (Sh * Iv) - (gamma * Ih)
+      dRh <-  (gamma * Ih)
+      
+      Nv <- Sv + Iv
+      
+      dSv <-  (-((betav * bitting)/Nh) * (Sv * Ih))
+      dIv <-  (((betav * bitting)/Nh) * (Sv * Ih))
+      
+      return(list(c(dSh, dIh, dRh, dSv, dIv), Nh = Nh, Nv = Nv))
+   })
+}
+
+# Initial conditions of the population
+init <- c(Sh = Sh, Ih = Ih, Rh = Rh, Sv = Sv, Iv = Iv)
+parameters <- c(betah = betah, betav = betav, gamma = gamma, bitting = bitting)
+times <- seq(from = 0, to = steps, by = 1)
+
+simulation <- as.data.frame(lsoda(y = init, times = times, func = odeSIRSI, parms = parameters))
+head(simulation)
+tail(simulation)
+
+# Results
+if (output) {
+   if (outChart) {
+      plot(x = simulation$time, y = simulation$Sh, type = outChartStyle[1], col = outChartColor[1], xlab = "Time", ylab = "", ylim = c(0, max(simulation)))
+      lines(x = simulation$time, y = simulation$Ih, type = outChartStyle[2], col = outChartColor[2])
+      lines(x = simulation$time, y = simulation$Rh, type = outChartStyle[3], col = outChartColor[3])
+      lines(x = simulation$time, y = simulation$Sv, type = outChartStyle[4], col = outChartColor[4])
+      lines(x = simulation$time, y = simulation$Iv, type = outChartStyle[5], col = outChartColor[5])
+   }
+   if(outChartHuman) {
+      plot(x = simulation$time, y = simulation$Sh, type = outChartHumanStyle[1], col = outChartHumanColor[1], xlab = "Time", ylab = "Humans")
+      lines(x = simulation$time, y = simulation$Ih, type = outChartHumanStyle[2], col=outChartHumanColor[2])
+      lines(x = simulation$time, y = simulation$Rh, type = outChartHumanStyle[3], col=outChartHumanColor[3])
+      lines(x = simulation$time, y = simulation$Nh, type = outChartHumanStyle[4], col=outChartHumanColor[4], pch=20)
+   }
+   if(outChartVector) {
+      plot(x = simulation$time, y = simulation$Sv, type = outChartVectorStyle[1], col=outChartVectorColor[1], xlab="Time", ylab = "Vector")
+      lines(x = simulation$time, y = simulation$Iv, type = outChartVectorStyle[2], col=outChartVectorColor[2])
+      lines(x = simulation$time, y = simulation$Nv, type = outChartVectorStyle[3], col=outChartVectorColor[3], pch=20)
+   }
+   if (outTextScreen) {
+      lastRow <- simulation[nrow(simulation),]
+      row.names(lastRow) <- NULL
+      View(lastRow[ c("time", outTextScreenSelect) ])
+   }
+   if (outVisualTable)
+      View(simulation[ c("time", outVisualTableSelect) ] )
+   if (outLog)
+      write.table(x = simulation[ c("time", outLogSelect) ], file = outLogFile, append = !outLogOverwrite, quote = FALSE, sep = outLogSeparator, col.names = TRUE)
+}
