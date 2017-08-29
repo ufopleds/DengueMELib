0830d81d445fc51dd7b6d6884fdf0b5d30903235
fellipedoprado <fellipe.doprado@gmail.com>
2017-08-29 18:54:46 -0300

    A function must check the required dependencies of R models. Issue #29 (#31)
    A function must check the required dependencies of R models.
    
    The function was updated to cover the following situations: 
    1 - the model does not have any dependencies
    2 - the user has all dependencies and are all updated (equals or greater than the required version)
    3 - the user has an older version of one or more required dependencies
    4 - the user does not have one or more required dependencies, thus, they must be installed
    
    If the version of a installed package and that required by the model are different (cases 2 and 3), a warning is presented, and the user has to decide what he will do (e.g. manually install or remove packages).
    
    Closes #29

--- Models/transmission/r_seir_basic_model/r_seir_basic_model_input.R
+++ Models/transmission/r_seir_basic_model/r_seir_basic_model_input.R
@@ -23,7 +23,7 @@ RESULTS_PATH <- BASE_PATH
 USER_RVERSION <- package_version(R.version)
 USER_PACKINFO <- installed.packages(fields = c("Package", "Version"))
 source(paste0(BASE_PATH, "/../../dmelib.r"))
-dmelib_check_dependencies(MODEL_RVERSION)
+dmelib_check_dependencies(modelRVersion = MODEL_RVERSION, modelDependences = DEPENDENCIES, userRVersion = USER_RVERSION, userLibs = USER_PACKINFO)
 
 # PARAMETERS
 Sh <- 9999 # number of susceptible humans [1-999999999]
